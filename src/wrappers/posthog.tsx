1c42a3fda546afb3a1bc6ef738265f7b05b02694
Anurag Kanoria <anurag@fromtheculture.co>
2024-09-28 19:21:57 +0530

    v1

--- src/wrappers/posthog.tsx
+++ src/wrappers/posthog.tsx
@@ -2,8 +2,8 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  posthog.init("phc_IYG4cqZDkCi7RlkfoHtKULI0NbZZs9GIrLTggqjdlW2", {
-    api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+  posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+    api_host: import.meta.env.VITE_POSTHOG_HOST,
     person_profiles: "identified_only",
     capture_pageview: false,
   });
